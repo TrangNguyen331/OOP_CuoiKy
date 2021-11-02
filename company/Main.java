d4dbe430130e26a36462f79407805a24651d4cf1
nguyenduynguyen06 <93526207+nguyenduynguyen06@users.noreply.github.com>
2021-11-02 23:03:48 +0700

    Add files via upload

---
+++ company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Nguyen");
+        System.out.println(s.getName());
+    }
+}
