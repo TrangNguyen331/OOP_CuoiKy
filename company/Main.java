d2a4eaa481d214fc09cc9bc4732f8d8f6b7f9d8e
nguyenduynguyen05 <20110530@student.hcmute.edu.vn>
2021-11-02 23:16:04 +0700

    Mới sửa thử coi nó lên hong

--- company/Main.java
+++ company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 class Main {
     public static void main(String[] args) {
         Student s = new Student();
-        s.setName("Nguyen");
+        s.setName("Duong Ngu");
         System.out.println(s.getName());
     }
 }
