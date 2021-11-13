5c69e95dcc8a89de4fc6739b03064b679a900e15
QuangDuongLe <20110454@studnet.hcmute.edu.vn>
2021-11-13 23:57:33 +0700

    Chỉnh sửa

--- OOP_CuoiKy/src/Main.java
+++ OOP_CuoiKy/src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Student a = new Student("Quang Dương", 19
-                , 1.8f, "HCMUTE");
+        Student a = new Student("Quang Dương", 19, 1.8f, "HCMUTE");
         a.getInfo();
         a.activity();
-        Student1 b = new Student1("Thùy Trang", 19
-                , 1.6f, "HCMUTE" );
+        Student1 b = new Student1("Thùy Trang", 19, 1.6f, "HCMUTE" );
         b.getInfo();
         b.activity();
-        Student2 c = new Student2("Duy Nguyễn",19,1.6f,"HCMUTE");
+        Student2 c = new Student2("Duy Nguyễn",19, 1.7f,"HCMUTE");
         c.getInfo();
         c.activity();
     }
