cd0a56f3df4c3c538d340c0060b71a7945f4defb
QuangDuongLe <93527004+QuangDuongLe@users.noreply.github.com>
2021-11-21 20:10:02 +0700

    Project Quản Lí Giao Dịch
    Tiến độ 1

---
+++ QuanLiGiaoDich/src/DanhSachGD.java
@@ -0,0 +1,8 @@
+public class DanhSachGD {
+    private GiaoDich[] danhSach;
+    private int count;
+    public DanhSachGD(int n){
+        danhSach = new GiaoDich[n];
+        count = 0;
+    }
+}
---
+++ QuanLiGiaoDich/src/GDVang.java
@@ -0,0 +1,10 @@
+public class GDVang extends GiaoDich{
+    private String loaiVang;
+
+    public String getLoaiVang() {
+        return loaiVang;
+    }
+    public void setLoaiVang(String loaiVang) {
+        this.loaiVang = loaiVang;
+    }
+}
